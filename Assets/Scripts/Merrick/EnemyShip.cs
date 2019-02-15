0e97b85eb3787b8ac86a7327d3563e3ca9b31245
Mickey1356 <jeremycwj@gmail.com>
2019-02-15 23:14:24 +0800

    Added proper images. Implemented Chaser enemy

--- Assets/Scripts/Merrick/EnemyShip.cs
+++ Assets/Scripts/Merrick/EnemyShip.cs
@@ -4,9 +4,27 @@ using UnityEngine;
 
 public class EnemyShip : Ship {
 
+    private GameObject _player;
+    public GameObject player {
+        get { return _player; }
+    }
+
+    private float _rotSpd, _moveSpd;
+    public float rotSpd {
+        get { return _rotSpd; }
+    }
+    public float moveSpd {
+        get { return _moveSpd; }
+    }
+
+    public void Initialise (GameObject player, float rotSpd, float moveSpd) {
+        this._player = player;
+        this._rotSpd = rotSpd;
+        this._moveSpd = moveSpd;
+    }
 
     public override void OnDeath() {
-        // spawn death expxlosion
+        // spawn death explosion
         Destroy(gameObject);
     }
 
--- Assets/Scripts/Merrick/PlayerShip.cs
+++ Assets/Scripts/Merrick/PlayerShip.cs
@@ -2,26 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShip : Ship {
-    
+public class PlayerShip : Ship
+{
+
     public static PlayerShip playerShip = null;
 
-    private void Start() {
+    // reference to sprites
+    public Sprite[] playerSprites;
+
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+    }
+
+    private void Start()
+    {
         if (playerShip != null) Destroy(playerShip);
         playerShip = this;
     }
 
-    public override void OnDeath() {
+    private void Update()
+    {
+        // alternate between the different images for thrusters
+        if (Input.GetAxis("p1 a") > 0 && Input.GetAxis("p2 a") == 0)
+        {
+            sprite.sprite = playerSprites[2];
+        }
+        else if (Input.GetAxis("p1 a") == 0 && Input.GetAxis("p2 a") > 0)
+        {
+            sprite.sprite = playerSprites[1];
+        }
+                else if (Input.GetAxis("p1 a") > 0 && Input.GetAxis("p2 a") > 0)
+        {
+            sprite.sprite = playerSprites[3];
+        }
+        else
+        {
+            sprite.sprite = playerSprites[0];
+        }
+
+    }
+
+    public override void OnDeath()
+    {
         // spawn explosions
         // trigger defeat screen
 
         //placeholder
-        SpriteRenderer sprite = GetComponent<SpriteRenderer>();
         if (sprite != null) sprite.enabled = false;
     }
 
-    public override bool ReceiveProjectile(Projectile p) {
-        if (!p.isPlayerProjectile) {
+    public override bool ReceiveProjectile(Projectile p)
+    {
+        if (!p.isPlayerProjectile)
+        {
             Damage(p.damage);
             Damage(p.damagePerSecond * Time.fixedDeltaTime);
             return true;
