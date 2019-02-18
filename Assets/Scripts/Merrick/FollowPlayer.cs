00f6875a3ae7df2679a10d5aa048ef3b21cb1b36
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-02-18 08:48:43 +0800

    made camera follow player without rotating with it

---
+++ Assets/Scripts/Merrick/FollowPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour {
+
+    [SerializeField] private GameObject player = null;
+
+    private void Update() {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+    }
+}
--- Assets/Scripts/Merrick/Thruster.cs
+++ Assets/Scripts/Merrick/Thruster.cs
@@ -2,27 +2,23 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Thruster : MonoBehaviour
-{
+public class Thruster : MonoBehaviour {
 
     public float thrust = 2;
     [System.NonSerialized] public new Rigidbody2D rigidbody;
 
     public string axisName = "";
 
-    private void Start()
-    {
+    private void Start() {
 
         rigidbody = transform.parent.GetComponent<Rigidbody2D>();
-        if (rigidbody == null)
-        {
+        if (rigidbody == null) {
             Debug.LogWarning("the parent of " + gameObject.name + " does not have a rigidbody, self-deleting");
             Destroy(this);
         }
     }
 
-    private void Update()
-    {
+    private void Update() {
         rigidbody.AddForceAtPosition(thrust * Input.GetAxis(axisName) * transform.up, transform.position, ForceMode2D.Force);
     }
 }
