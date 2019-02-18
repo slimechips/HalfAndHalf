d84d649790b765f1ded4a1d4254b03acebbdb9be
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-02-18 11:26:37 +0800

    Player gun and aim direction. Know glitch: when input is exactly opposite to side direction, gun will rotate out of the plane

---
+++ Assets/Scripts/Merrick/PlayerBullet.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : Bullet {
+
+    public float speed = 1;
+
+    private void FixedUpdate() {
+        transform.position += transform.up * Time.fixedDeltaTime * speed;
+    }
+}
---
+++ Assets/Scripts/Merrick/PlayerGun.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerGun : MonoBehaviour {
+
+    [SerializeField] private bool player1 = true;
+    [SerializeField] private PlayerBullet bullet = null;
+
+    [SerializeField] private float rotateAngle = 90;
+    [SerializeField] private float fireRate = 5;
+    private float cooldown = 0;
+    private float period = 10;
+
+    private void Start() {
+        period = 1 / fireRate;
+        if (bullet == null) {
+            Debug.Log("no bullet prefab");
+            this.enabled = false;
+        }
+    }
+
+    private void Update() {
+        if (player1) {
+            transform.up = Vector3.RotateTowards(-transform.parent.right, 
+                                                 new Vector3(Input.GetAxis("p1 left horizontal"), Input.GetAxis("p1 left vertical"), 0).normalized, 
+                                                 Mathf.Deg2Rad * rotateAngle, 0);
+        }
+        else {
+            transform.up = Vector3.RotateTowards(transform.parent.right,
+                                                 new Vector3(Input.GetAxis("p2 left horizontal"), Input.GetAxis("p2 left vertical"), 0).normalized,
+                                                 Mathf.Deg2Rad * rotateAngle, 0);
+        }
+    }
+
+    private void FixedUpdate() {
+        if (cooldown > 0) {
+            cooldown -= Time.fixedDeltaTime;
+        }
+        else if (player1) {
+            if (Input.GetButton("p1 b")) {
+                cooldown += period;
+                Instantiate(bullet, transform.position, transform.rotation);
+                //bullet.transform.localRotation = transform.localRotation;
+            }
+        }
+        else {
+            if (Input.GetButton("p2 b")) {
+                cooldown += period;
+                Instantiate(bullet, transform.position, transform.rotation);
+                //bullet.transform.localRotation = transform.localRotation;
+            }
+        }
+    }
+}
