4df8d855e1de602bbaf7d261cadcb25668ed6976
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-02-21 19:28:47 +0800

    player bullets fizzle after 5 seconds

--- Assets/Scripts/Merrick/PlayerBullet.cs
+++ Assets/Scripts/Merrick/PlayerBullet.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerBullet : Bullet {
 
     public float speed = 1;
+    private float duration = 5f;
 
     private void FixedUpdate() {
+        duration -= Time.deltaTime;
+        if (duration < 0) Destroy(gameObject);
         transform.position += transform.up * Time.fixedDeltaTime * speed;
     }
 }
