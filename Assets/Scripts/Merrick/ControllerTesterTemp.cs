bb887781f7440129367ced2a5fec8581c7988b62
FlyingFactory <36451516+FlyingFactory@users.noreply.github.com>
2019-02-21 11:37:59 +0800

    checked joystick controls and fixed inputs

---
+++ Assets/Scripts/Merrick/ControllerTesterTemp.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerTesterTemp : MonoBehaviour {
+
+    private void Update() {
+        string pressedbuttons = "";
+
+        List<string> axes = new List<string>() {
+            "p1 left horizontal",
+            "p1 left vertical",
+            "p1 right horizontal",
+            "p1 right vertical",
+            "p1 a",
+            "p1 b",
+            "p1 x",
+            "p1 y",
+        };
+
+        foreach(string s in axes) {
+            if (Mathf.Abs(Input.GetAxis(s)) > 0.1f) pressedbuttons += s + ":" + Input.GetAxis(s) + ", ";
+        }
+
+        Debug.Log(pressedbuttons);
+    }
+}
