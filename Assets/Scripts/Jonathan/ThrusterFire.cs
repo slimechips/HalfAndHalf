055b3f773d3abecc3b48ad6581340b405a290d58
JWIGGS <destiny.jonathan@gmail.com>
2019-02-21 22:33:42 +0800

    added some sprites

---
+++ Assets/Scripts/Jonathan/ThrusterFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrusterFire : MonoBehaviour
+{
+
+    public string axisName = "";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    private void Update()
+    {
+        bool v = (Input.GetAxis(axisName) > 0);
+        GetComponent<SpriteRenderer>().color = new Color(255, 255, 255, v? 1f:0f);
+
+   }
+
+}
