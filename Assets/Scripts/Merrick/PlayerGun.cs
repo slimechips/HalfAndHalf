f4a0db48b3561ec0507d1ef4874e06cbbf0b366c
SilverAlfredo <49393621+SilverAlfredo@users.noreply.github.com>
2020-01-28 23:53:58 +0800

    control change
    changed a to rb, was experimenting with changing the buttons for the combos but rb actually isn't as bad as a so kept them the same

--- Assets/Scripts/Merrick/PlayerGun.cs
+++ Assets/Scripts/Merrick/PlayerGun.cs
@@ -20,12 +20,6 @@ public class PlayerGun : MonoBehaviour {
             else if (ComboManager.State == ComboManager.ComboState.ShipOfTheLine) {
                 return 0.04f;
             }
-            else if (player1 && ComboManager.State == ComboManager.ComboState.SwordAndBoardP1) {
-                return 0.04f;
-            }
-            else if (!player1 && ComboManager.State == ComboManager.ComboState.SwordAndBoardP2) {
-                return 0.04f;
-            }
             else return _period;
         }
         set {
@@ -71,7 +65,7 @@ public class PlayerGun : MonoBehaviour {
         }
         else if (ComboManager.State == ComboManager.ComboState.ShieldStrike) {; }
         else if (player1) {
-            if ((Input.GetButton("p1 a") && !Input.GetButton("p1 x"))
+            if ((Input.GetButton("p1 rb") && !Input.GetButton("p1 x"))
                  || ComboManager.State == ComboManager.ComboState.BulletSpinClock
                  || ComboManager.State == ComboManager.ComboState.BulletSpinAntiC
                  || ComboManager.State == ComboManager.ComboState.ShipOfTheLine) {
@@ -80,7 +74,7 @@ public class PlayerGun : MonoBehaviour {
             }
         }
         else {
-            if ((Input.GetButton("p2 a") && !Input.GetButton("p2 x"))
+            if ((Input.GetButton("p2 rb") && !Input.GetButton("p2 x"))
                  || ComboManager.State == ComboManager.ComboState.BulletSpinClock
                  || ComboManager.State == ComboManager.ComboState.BulletSpinAntiC
                  || ComboManager.State == ComboManager.ComboState.ShipOfTheLine) {
